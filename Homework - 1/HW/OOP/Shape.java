d4df1bca95bbeb3f57245f4ad463534f3b95aa2d
catherine20116 <yekaterina.kurochkina.b@gmail.com>
2021-09-17 13:56:04 +0300

    OOP 3

---
+++ Homework - 1/HW/OOP/Shape.java
@@ -0,0 +1,18 @@
+
+public class Shape {
+	public String color;
+
+	public String getColor() {
+		return color;
+	}
+	
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
