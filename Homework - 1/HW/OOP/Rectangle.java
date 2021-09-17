151147f243d2efeef43bfbc892ca09b49b4a96bb
catherine20116 <yekaterina.kurochkina.b@gmail.com>
2021-09-17 11:57:08 +0300

    OOP 1

---
+++ Homework - 1/HW/OOP/Rectangle.java
@@ -0,0 +1,49 @@
+
+public class Rectangle {
+	int length;
+	int width;
+	String kvadrats;
+	
+	public Rectangle(int length, int width) {
+		this.length = length;
+		this.width = width;
+		this.kvadrats = isSquare();
+	}
+	
+	public Rectangle(int length) {
+		this.length = length;
+		this.width = length;
+		this.kvadrats = isSquare();
+	}
+	
+	public String isSquare() {
+		if (length == width) {
+			return "Is Square";
+		} else {
+			return "Is Not Square";
+		}
+	}
+	
+	public int area() {
+		return this.length * this.width;
+	}
+	
+	public int perimeter() {
+		return (this.length + this.width)*2;
+	}
+	
+	public String toString() {
+		return "This form " + isSquare() + " with length " + this.length + " and with width " + this.width + ". It's area equals to " + area() + " and it's perimeter equals to " + perimeter() + "."; 
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Rectangle one = new Rectangle (5, 4);
+		System.out.println(one);
+		Rectangle two = new Rectangle (3, 8);
+		System.out.println(two);
+		Rectangle three = new Rectangle (6);
+		System.out.println(three);
+	}
+
+}
